d553b9a01096fe11cc1f52108a0fc3710bbfc6c2
HurtubiseJ <jhurtubise23@gmail.com>
2024-08-25 12:10:45 -0400

    Base DB Stores and Procedures created

---
+++ DB/StoreTime.sql
@@ -0,0 +1,57 @@
+USE [GreatLakesWaterLevels]
+GO
+/****** Object:  StoredProcedure [dbo].[StoreTime]    Script Date: 8/25/2024 11:08:41 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[StoreTime]
+	@StationId NVARCHAR(30),
+	@Date DATETIME, 
+	@V NVARCHAR(20), 
+	@S NVARCHAR(20), 
+	@F NVARCHAR(5), 
+	@Q NVARCHAR(5)
+AS 
+BEGIN 
+	SET NOCOUNT ON;
+	IF @StationId is null
+		BEGIN 
+			RAISERROR('StationId cannot be NULL', 16, 1)
+			RETURN 
+		END
+
+	IF @Date is null
+		BEGIN 
+			RAISERROR('Date cannot be NULL', 16, 1) 
+			RETURN 
+		END
+
+	BEGIN TRY
+		BEGIN TRANSACTION; 
+
+		IF OBJECT_ID('@StationId', 'U') is null
+			BEGIN 
+				DECLARE @SQL NVARCHAR(MAX) 
+				SET @SQL = 'CREATE TABLE ' + '@StationId' + ' (
+					Date DATETIME, 
+					V NVARCHAR(20), 
+					S NVARCHAR(20), 
+					F NVARCHAR(10), 
+					Q NVARCHAR(5)
+				);'
+				EXEC sp_executesql @SQL, N'@StationId NVARCHAR(30)', @StationId
+			END;
+		ELSE
+			BEGIN
+				DECLARE @SQL2 NVARCHAR(MAX) 
+				SET @SQL2 = 'INSERT INTO ' + @StationId + ' VALUES (@Date, @V, @S, @f, @Q)'
+				EXEC sp_executesql @SQL2, N'@Date NVARCHAR(30), @V NVARCHAR(20), @S NVARCHAR(20), @F NVARCHAR(10), @Q NVARCHAR(5)', @Date, @V, @S, @F, @Q
+			END;
+	COMMIT TRANSACTION;
+	END TRY 
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		THROW; 
+	END CATCH 
+END;
--- info_pull.py
+++ info_pull.py
@@ -4,19 +4,20 @@ import requests
 from requests.models import PreparedRequest
 import pandas as pd 
 import pathlib
+import pyodbc as pdb
 
 #https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=hourly_height&application=NOS.COOPS.TAC.WL&begin_date=20230101&end_date=20240101&datum=IGLD&station=9075002&time_zone=LST&units=english&format=json
 BASE_URL = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?"
-FORMAT = 'json'
+FORMAT = "json"
 TIME_ZONE = 'LST'
-APPLICATION = 'hurtubisej'
-DATUM = 'IGLD'
+APPLICATION = 'HurtubiseJ'
+DATUM = "IGLD"
+UNITS = "english"
 
 stations_json_path = pathlib.Path('C:\\Users\\jhurt\\OneDrive\\Desktop\\Great_Lakes_Water_Tracker\\stations.json')
 
 def get_recent_data(station='all'):
-    response = requests.get('https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=hourly_height&application=NOS.COOPS.TAC.WL&begin_date=20230101&end_date=20240101&datum=IGLD&station=9075002&time_zone=LST&units=english&format=json')
-    return response
+    pass 
 
 def stationId_by_region(station_json_content, selected_region):
     list = []
@@ -33,16 +34,42 @@ def load_stations_json(path):
     except json.JSONDecodeError as e:
         print(f'Could not Load stations.json file: {e}')
 
+def create_DB_cursor():
+    try:
+        conn = pdb.connect(CONNECTION_STRING)
+        cursor = conn.cursor()
+        print("Database connection and cursor successfully created.")
+        return conn, cursor
+    except pdb.Error as e:
+        print(f"Failed to connect to database: {e}")
+        return None, None
+
+def call_procedure_StoreTime(StationId, Date, V, S, F, Q):
+    params = (
+        StationId, Date, V, S, F, Q
+    )
+    SQL = """
+        EXEC StoreTime
+            @StationId = ?, @Date = ?, @V = ?, @S = ?, @F = ?, @Q = ?
+    """
+    # try:
+    print(params)
+    cursor.execute(SQL, params)
+    cursor.commit()
+    # except:
+    #     print("Failed to execute stored procedure 'StoreTime'.")
+    #     print(params)
+
 def recent_data_params(Id):
     params = {
         "date":"today",
         "product":"water_level",
         "station":f"{Id}",
-        "datum":"IGLD",
-        "units":"english",
+        "datum":f"{DATUM}",
+        "units":f"{UNITS}",
         "time_zone":"lst",
-        "format":"json",
-        "application":"HurtubiseJ"
+        "format":f"{FORMAT}",
+        "application":f"{APPLICATION}"
     }
     return params
 
@@ -62,9 +89,19 @@ def pull_recent_data_from_Id(Id):
     except:
         print(f"Failed to pull recent data for stationId: {Id}")
 
+CONNECTION_STRING = ("DRIVER={SQL Server};""SERVER=DESKTOP-7H6LSQ4;""DATABASE=GreatLakesWaterLevels;""TrustedServerConnection=Yes;")
+conn, cursor = None, None
+conn, cursor = create_DB_cursor()
+
+def df_to_db(StationId, df):
+    for index, row in df.iterrows():
+        call_procedure_StoreTime(StationId, row['t'], row['v'], row['s'], row['f'], row['q'])
+
 def main():
     df = pull_recent_data_from_Id("9044030")
-    print(df)
-    
+    StationId = "9044030"
+    # print(df)
+    df_to_db(StationId, df)
+
 if __name__ == '__main__':
     main()
